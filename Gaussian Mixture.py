96d8498757aed3b928cfcf8cea5edb7ccc126b93
Alex Polotsk <>
2021-10-07 08:22:10 +0200

    Transpose axes from (z,x) to (x,z)

--- Gaussian Mixture.py
+++ Gaussian Mixture.py
@@ -9,26 +9,26 @@ def gauss(x, mean, variance):
   from numpy import pi, sqrt, exp
   return 1/sqrt(2*pi*variance) * exp(-(x-mean)**2/(2*variance))
 
-z_length, x_length = 2, 1000
+x_length, z_length = 1000, 2
 def target_θ():
-  P_z = np.expand_dims([0.7, 0.3], axis=1)
-  means = np.expand_dims([8.0, 13.0], axis=1)
-  variances = np.expand_dims([2.0, 1.0], axis=1)
+  P_z = np.expand_dims([0.7, 0.3], axis=0)
+  means = np.expand_dims([8.0, 13.0], axis=0)
+  variances = np.expand_dims([2.0, 1.0], axis=0)
   return P_z, means, variances
 target_θ = target_θ()
 
 def x(target_θ):
   P_z, means, variances = target_θ
-  z = np.random.choice(z_length, size=x_length, p=P_z[:,0])
-  samples = np.random.normal(means, variances**0.5, [z_length, x_length])
-  return np.choose(z, samples)
+  z = np.random.choice(z_length, size=x_length, p=P_z[0])
+  samples = np.random.normal(means, variances**0.5, [x_length, 1, z_length])
+  return np.choose(z, samples.T).T
 x = x(target_θ)
 
 def θ():
-  def normalize(x, axis=0): return x/x.sum(axis=axis, keepdims=True)
-  P_z = normalize(np.random.rand(z_length, 1)*0.5+0.25)
-  means = np.random.rand(z_length, 1)*20-10
-  variances = np.random.rand(z_length, 1)*10+1
+  def normalize(x, axis=1): return x/x.sum(axis=axis, keepdims=True)
+  P_z = normalize(np.random.rand(1, z_length)*0.5+0.25)
+  means = np.random.rand(1, z_length)*20-10
+  variances = np.random.rand(1, z_length)*10+1
   return P_z, means, variances
 θ = θ()
 
@@ -38,16 +38,16 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
     P_z, means, variances = θ
     P_x_given_z = gauss(x, means, variances)
     P_x_and_z = P_z*P_x_given_z
-    P_x = P_x_and_z.sum(axis=0, keepdims=True)
+    P_x = P_x_and_z.sum(axis=1, keepdims=True)
     P_z_given_x = P_x_and_z/P_x
     return P_z_given_x
   P_z_given_x = expectation_step(x, θ)
 
   def maximization_step(P_z_given_x, x):
-    w = P_z_given_x.sum(axis=1, keepdims=True)
+    w = P_z_given_x.sum(axis=0, keepdims=True)
     P_z = w/len(x)
-    means = (P_z_given_x*x).sum(axis=1, keepdims=True)/w
-    variances = (P_z_given_x*(x-means)**2).sum(axis=1, keepdims=True)/w
+    means = (P_z_given_x*x).sum(axis=0, keepdims=True)/w
+    variances = (P_z_given_x*(x-means)**2).sum(axis=0, keepdims=True)/w
     return P_z, means, variances
   θ = maximization_step(P_z_given_x, x)
 
@@ -62,12 +62,12 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
     weights = np.ones_like(x)/len(x)/bin_size
     pyplot.hist(x, bins=bins, weights=weights, color='g', alpha=0.3, label='Samples')
 
-    x = np.linspace(x.min(), x.max(), 1000)
+    x = np.linspace([x.min()], [x.max()], 1000)
     def P_x(x, θ):
       P_z, means, variances = θ
       P_x_given_z = gauss(x, means, variances)
       P_x_and_z = P_z*P_x_given_z
-      P_x = P_x_and_z.sum(axis=0)
+      P_x = P_x_and_z.sum(axis=1, keepdims=True)
       return P_x
     y = P_x(x, target_θ)
     pyplot.plot(x, y, 'g-', alpha=0.3, label='P(x)')
@@ -76,9 +76,9 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
     pyplot.plot(x, y, 'b-', label='P(x|θ)')
 
     P_z, means, variances = θ
-    y = P_x(means.T, θ)[None].T
-    pyplot.plot((means + [-1,1]*variances**0.5).T, (y/[2,2]).T, '|-b', alpha=0.3, linewidth=1)
-    pyplot.plot(([1,1]*means).T, ([0,1]*y).T, '|-b', alpha=0.3, linewidth=1)
+    y = P_x(means.T, θ).T
+    pyplot.plot(means + [[-1],[1]]*variances**0.5, y/[[2],[2]], '|-b', alpha=0.3, linewidth=1)
+    pyplot.plot([[1],[1]]*means, [[0],[1]]*y, '|-b', alpha=0.3, linewidth=1)
     pyplot.show(block=False)
     pyplot.pause(0.01)
   show(x)
@@ -87,6 +87,6 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
     P_z, means, variances = θ
     P_x_given_z = gauss(x, means, variances)
     P_x_and_z = P_z*P_x_given_z
-    P_x = P_x_and_z.sum(axis=0)
+    P_x = P_x_and_z.sum(axis=1)
     return np.log(P_x).sum(axis=0)
   log_likelihood1, log_likelihood0 = log_likelihood(x, θ), log_likelihood1
