53aa6147380b42adc56ba37e8d10f679ef8ba8bf
Alex Polotsk <apolotsk@gmail.com>
2021-11-20 11:01:57 +0100

    Transpose axes from (x,z) to (z,x)

--- Gaussian Mixture.pytorch.py
+++ Gaussian Mixture.pytorch.py
@@ -7,27 +7,27 @@ np.random.seed(0)
 import torch
 from torch import tensor
 
-x_length, z_length = 1000, 2
+z_length, x_length = 2, 1000
 def target_θ():
-  P_z = tensor(np.expand_dims([0.7, 0.3], axis=0))
-  means = tensor(np.expand_dims([8.0, 13.0], axis=0))
-  stdevs = tensor(np.expand_dims([1.4, 1.0], axis=0))
+  P_z = tensor(np.expand_dims([0.7, 0.3], axis=1))
+  means = tensor(np.expand_dims([8.0, 13.0], axis=1))
+  stdevs = tensor(np.expand_dims([1.4, 1.0], axis=1))
   return P_z, means, stdevs
 target_θ = target_θ()
 
 def x(target_θ):
   P_z, means, stdevs = target_θ
   P_z, means, stdevs = P_z.numpy(), means.numpy(), stdevs.numpy()
-  z = np.random.choice(z_length, size=x_length, p=P_z[0])
-  samples = np.random.normal(means, stdevs, [x_length, 1, z_length])
-  x = np.choose(z, samples.T).T
+  z = np.random.choice(z_length, size=x_length, p=P_z[:,0])
+  samples = np.random.normal(means, stdevs, [z_length, x_length])
+  x = np.choose(z, samples)
   return tensor(x)
 x = x(target_θ)
 
 def θ():
-  P_z = tensor(np.ones([1, z_length])/z_length, requires_grad=True)
-  means = tensor(np.random.rand(1, z_length)*20, requires_grad=True)
-  stdevs = tensor(np.random.rand(1, z_length)*5, requires_grad=True)
+  P_z = tensor(np.ones([z_length, 1])/z_length, requires_grad=True)
+  means = tensor(np.random.rand(z_length, 1)*20, requires_grad=True)
+  stdevs = tensor(np.random.rand(z_length, 1)*5, requires_grad=True)
   return P_z, means, stdevs
 θ = θ()
 
@@ -40,17 +40,17 @@ log_likelihood1, log_likelihood0 = float('inf'), float('-inf')
 while abs(log_likelihood1-log_likelihood0)>1e-6:
   def P_x(x, θ):
     P_z, means, stdevs = θ
-    P_z = P_z/P_z.sum(dim=1, keepdim=True)
+    P_z = P_z/P_z.sum(dim=0, keepdim=True)
     def gauss(x, mean, stdev):
       from math import pi
       from torch import sqrt, exp
       return 1/sqrt(2*pi*stdev**2) * exp(-(x-mean)**2/(2*stdev**2))
     P_x_given_z = gauss(x, means, stdevs)
-    P_x_and_z = P_z*P_x_given_z
-    P_x = P_x_and_z.sum(dim=1, keepdim=True)
+    P_z_and_x = P_z*P_x_given_z
+    P_x = P_z_and_x.sum(dim=0)
     return P_x
   def log_likelihood(x, θ):
-    return torch.log(P_x(x, θ)[:,0]).sum(dim=0)
+    return torch.log(P_x(x, θ)).sum(dim=0)
   log_likelihood1, log_likelihood0 = log_likelihood(x, θ), log_likelihood1
 
   def update_parameters():
@@ -70,7 +70,7 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
     x = x.numpy()
     pyplot.hist(x, bins=bins, weights=weights, color='g', alpha=0.3, label='Samples')
 
-    x = np.linspace([x.min()], [x.max()], 1000)
+    x = np.linspace(x.min(), x.max(), 1000)
     def _P_x(x, θ):
       with torch.no_grad(): return P_x(tensor(x), θ).numpy()
     y = _P_x(x, target_θ)
@@ -80,8 +80,9 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
     pyplot.plot(x, y, 'b-', label='P(x|θ)')
 
     P_z, means, stdevs = θ
-    means, stdevs = means.detach().numpy(), stdevs.detach().numpy()
-    y = _P_x(means.T, θ).T
+    means, stdevs = means.T, stdevs.T
+    with torch.no_grad(): means, stdevs = means.numpy(), stdevs.numpy()
+    y = _P_x(means, θ)
     pyplot.plot(means + [[-1],[1]]*stdevs, y/[[2],[2]], '|-b', alpha=0.3, linewidth=1)
     pyplot.plot([[1],[1]]*means, [[0],[1]]*y, '|-b', alpha=0.3, linewidth=1)
 
