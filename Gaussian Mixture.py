7ac4b157b8dc5dad016444eff06747461d62b795
Alex Polotsk <apolotsk@gmail.com>
2021-11-21 11:50:24 +0100

    Vectorize

--- Gaussian Mixture.py
+++ Gaussian Mixture.py
@@ -3,53 +3,52 @@ Gaussian mixture with 2 normal distribution.
 http://courses.cs.washington.edu/courses/cse312/11wi/slides/12em.pdf
 """
 import numpy as np
-np.random.seed(1)
+np.random.seed(0)
 
 def gauss(x, mean, variance):
   from numpy import pi, sqrt, exp
   return 1/sqrt(2*pi*variance) * exp(-(x-mean)**2/(2*variance))
 
+z_length, x_length = 2, 1000
 def target_θ():
-  P_z0, mean0, variance0 = 0.7, 8.0, 2.0
-  P_z1, mean1, variance1 = (1-P_z0), 13.0, 1.0
-  return (P_z0, mean0, variance0), (P_z1, mean1, variance1)
+  P_z = np.expand_dims([0.7, 0.3], axis=1)
+  means = np.expand_dims([8.0, 13.0], axis=1)
+  variances = np.expand_dims([2.0, 1.0], axis=1)
+  return P_z, means, variances
 target_θ = target_θ()
 
 def x(target_θ):
-  (P_z0, mean0, variance0), (P_z1, mean1, variance1) = target_θ
-  def sample():
-    import numpy as np
-    if np.random.rand()<P_z0: return np.random.normal(mean0, variance0**0.5)
-    else: return np.random.normal(mean1, variance1**0.5)
-  return np.array([sample() for _ in range(1000)])
+  P_z, means, variances = target_θ
+  z = np.random.choice(z_length, size=x_length, p=P_z[:,0])
+  samples = np.random.normal(means, variances**0.5, [z_length, x_length])
+  return np.choose(z, samples)
 x = x(target_θ)
 
 def θ():
-  from numpy.random import rand
-  P_z0, mean0, variance0 = rand()*0.5+0.25, rand()*20-10, rand()*10+1
-  P_z1, mean1, variance1 = (1-P_z0), rand()*20-10, rand()*10+1
-  return (P_z0, mean0, variance0), (P_z1, mean1, variance1)
+  def normalize(x, axis=0): return x/x.sum(axis=axis, keepdims=True)
+  P_z = normalize(np.random.rand(z_length, 1)*0.5+0.25)
+  means = np.random.rand(z_length, 1)*20-10
+  variances = np.random.rand(z_length, 1)*10+1
+  return P_z, means, variances
 θ = θ()
 
 log_likelihood1, log_likelihood0 = float('inf'), float('-inf')
 while abs(log_likelihood1-log_likelihood0)>1e-6:
   def expectation_step(x, θ):
-    P_x_given_z = [[gauss(x, mean, variance) for P_z, mean, variance in θ] for x in x]
-    P_x_and_z = [[P_z*P_xi_given_zj for (P_z,_,_), P_xi_given_zj in zip(θ, P_xi_given_z)] for P_xi_given_z in P_x_given_z]
-    P_x = [sum(P_xi_and_z) for P_xi_and_z in P_x_and_z]
-    P_z_given_x = [[P_xi_and_zj/P_xi for P_xi_and_zj in P_xi_and_z] for P_xi_and_z, P_xi in zip(P_x_and_z, P_x)]
+    P_z, means, variances = θ
+    P_x_given_z = gauss(x, means, variances)
+    P_x_and_z = P_z*P_x_given_z
+    P_x = P_x_and_z.sum(axis=0, keepdims=True)
+    P_z_given_x = P_x_and_z/P_x
     return P_z_given_x
   P_z_given_x = expectation_step(x, θ)
 
   def maximization_step(P_z_given_x, x):
-    P_z_given_x = list(zip(*P_z_given_x))
-    def update(P_zi_given_x):
-      P_zi = sum(P_zi_given_x)/len(x)
-      mean = sum(P_zi_given_xj*x for P_zi_given_xj, x in zip(P_zi_given_x, x))/sum(P_zi_given_x)
-      variance = sum(P_zi_given_xj*(x-mean)**2 for P_zi_given_xj, x in zip(P_zi_given_x, x))/sum(P_zi_given_x)
-      return P_zi, mean, variance
-    θ = [update(P_zi_given_x) for P_zi_given_x in P_z_given_x]
-    return θ
+    w = P_z_given_x.sum(axis=1, keepdims=True)
+    P_z = w/len(x)
+    means = (P_z_given_x*x).sum(axis=1, keepdims=True)/w
+    variances = (P_z_given_x*(x-means)**2).sum(axis=1, keepdims=True)/w
+    return P_z, means, variances
   θ = maximization_step(P_z_given_x, x)
 
   def show(x):
@@ -62,27 +61,32 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
     bins = np.arange(x.min(), x.max(), bin_size)
     weights = np.ones_like(x)/len(x)/bin_size
     pyplot.hist(x, bins=bins, weights=weights, color='g', alpha=0.3, label='Samples')
+
     x = np.linspace(x.min(), x.max(), 1000)
-    def P_xi(x, θ):
-      P_xi_given_z = [gauss(x, mean, variance) for P_z, mean, variance in θ]
-      P_xi_and_z = [P_z*P_xi_given_zj for (P_z,_,_), P_xi_given_zj in zip(θ, P_xi_given_z)]
-      P_xi = sum(P_xi_and_z)
-      return P_xi
-    y = P_xi(x, target_θ)
+    def P_x(x, θ):
+      P_z, means, variances = θ
+      P_x_given_z = gauss(x, means, variances)
+      P_x_and_z = P_z*P_x_given_z
+      P_x = P_x_and_z.sum(axis=0)
+      return P_x
+    y = P_x(x, target_θ)
     pyplot.plot(x, y, 'g-', alpha=0.3, label='P(x)')
-    y = P_xi(x, θ)
+
+    y = P_x(x, θ)
     pyplot.plot(x, y, 'b-', label='P(x|θ)')
-    for P_zi, mean, variance in θ:
-      y = P_xi(mean, θ)
-      pyplot.plot([mean-variance**0.5, mean+variance**0.5], [y/2, y/2], '|-b', alpha=0.3, linewidth=1)
-      pyplot.plot([mean, mean], [0, y], '|-b', alpha=0.3, linewidth=1)
+
+    P_z, means, variances = θ
+    y = P_x(means.T, θ)[None].T
+    pyplot.plot((means + [-1,1]*variances**0.5).T, (y/[2,2]).T, '|-b', alpha=0.3, linewidth=1)
+    pyplot.plot(([1,1]*means).T, ([0,1]*y).T, '|-b', alpha=0.3, linewidth=1)
     pyplot.show(block=False)
     pyplot.pause(0.01)
   show(x)
 
   def log_likelihood(x, θ):
-    P_x_given_z = [[gauss(x, mean, variance) for P_z, mean, variance in θ] for x in x]
-    P_x_and_z = [[P_z*P_xi_given_zj for (P_z,_,_), P_xi_given_zj in zip(θ, P_xi_given_z)] for P_xi_given_z in P_x_given_z]
-    from math import log
-    return sum([log(sum(P_xi_and_z)) for P_xi_and_z in P_x_and_z])
+    P_z, means, variances = θ
+    P_x_given_z = gauss(x, means, variances)
+    P_x_and_z = P_z*P_x_given_z
+    P_x = P_x_and_z.sum(axis=0)
+    return np.log(P_x).sum(axis=0)
   log_likelihood1, log_likelihood0 = log_likelihood(x, θ), log_likelihood1
