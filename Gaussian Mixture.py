cce7fdfe549cdd79e9ac6c1739026c0d339b0278
Alex Polotsk <apolotsk@gmail.com>
2021-11-21 11:51:24 +0100

    Transpose axes from (x,z) to (z,x)

--- Gaussian Mixture.py
+++ Gaussian Mixture.py
@@ -9,25 +9,25 @@ def gauss(x, mean, stdev):
   from numpy import pi, sqrt, exp
   return 1/sqrt(2*pi*stdev**2) * exp(-(x-mean)**2/(2*stdev**2))
 
-x_length, z_length = 1000, 2
+z_length, x_length = 2, 1000
 def target_θ():
-  P_z = np.expand_dims([0.7, 0.3], axis=0)
-  means = np.expand_dims([8.0, 13.0], axis=0)
-  stdevs = np.expand_dims([1.4, 1.0], axis=0)
+  P_z = np.expand_dims([0.7, 0.3], axis=1)
+  means = np.expand_dims([8.0, 13.0], axis=1)
+  stdevs = np.expand_dims([1.4, 1.0], axis=1)
   return P_z, means, stdevs
 target_θ = target_θ()
 
 def x(target_θ):
   P_z, means, stdevs = target_θ
-  z = np.random.choice(z_length, size=x_length, p=P_z[0])
-  samples = np.random.normal(means, stdevs, [x_length, 1, z_length])
-  return np.choose(z, samples.T).T
+  z = np.random.choice(z_length, size=x_length, p=P_z[:,0])
+  samples = np.random.normal(means, stdevs, [z_length, x_length])
+  return np.choose(z, samples)
 x = x(target_θ)
 
 def θ():
-  P_z = np.ones([1, z_length])/z_length
-  means = np.random.rand(1, z_length)*20
-  stdevs = np.random.rand(1, z_length)*5
+  P_z = np.ones([z_length, 1])/z_length
+  means = np.random.rand(z_length, 1)*20
+  stdevs = np.random.rand(z_length, 1)*5
   return P_z, means, stdevs
 θ = θ()
 
@@ -36,25 +36,25 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
   def expectation_step(x, θ):
     P_z, means, stdevs = θ
     P_x_given_z = gauss(x, means, stdevs)
-    P_x_and_z = P_z*P_x_given_z
-    P_x = P_x_and_z.sum(axis=1, keepdims=True)
-    P_z_given_x = P_x_and_z/P_x
+    P_z_and_x = P_z*P_x_given_z
+    P_x = P_z_and_x.sum(axis=0, keepdims=True)
+    P_z_given_x = P_z_and_x/P_x
     return P_z_given_x
   P_z_given_x = expectation_step(x, θ)
 
   def maximization_step(P_z_given_x, x):
-    w = P_z_given_x.sum(axis=0, keepdims=True)
+    w = P_z_given_x.sum(axis=1, keepdims=True)
     P_z = w/len(x)
-    means = (P_z_given_x*x).sum(axis=0, keepdims=True)/w
-    stdevs = np.sqrt((P_z_given_x*(x-means)**2).sum(axis=0, keepdims=True)/w)
+    means = (P_z_given_x*x).sum(axis=1, keepdims=True)/w
+    stdevs = np.sqrt((P_z_given_x*(x-means)**2).sum(axis=1, keepdims=True)/w)
     return P_z, means, stdevs
   θ = maximization_step(P_z_given_x, x)
 
   def P_x(x, θ):
     P_z, means, stdevs = θ
     P_x_given_z = gauss(x, means, stdevs)
-    P_x_and_z = P_z*P_x_given_z
-    P_x = P_x_and_z.sum(axis=1, keepdims=True)
+    P_z_and_x = P_z*P_x_given_z
+    P_x = P_z_and_x.sum(axis=0)
     return P_x
 
   def show(x):
@@ -67,7 +67,7 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
     weights = np.ones_like(x)/len(x)/bin_size
     pyplot.hist(x, bins=bins, weights=weights, color='g', alpha=0.3, label='Samples')
 
-    x = np.linspace([x.min()], [x.max()], 1000)
+    x = np.linspace(x.min(), x.max(), 1000)
     y = P_x(x, target_θ)
     pyplot.plot(x, y, 'g-', alpha=0.3, label='P(x)')
 
@@ -75,7 +75,8 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
     pyplot.plot(x, y, 'b-', label='P(x|θ)')
 
     P_z, means, stdevs = θ
-    y = P_x(means.T, θ).T
+    means, stdevs = means.T, stdevs.T
+    y = P_x(means, θ)
     pyplot.plot(means + [[-1],[1]]*stdevs, y/[[2],[2]], '|-b', alpha=0.3, linewidth=1)
     pyplot.plot([[1],[1]]*means, [[0],[1]]*y, '|-b', alpha=0.3, linewidth=1)
 
@@ -84,5 +85,5 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
   show(x)
 
   def log_likelihood(x, θ):
-    return np.log(P_x(x, θ)[:,0]).sum(axis=0)
+    return np.log(P_x(x, θ)).sum(axis=0)
   log_likelihood1, log_likelihood0 = log_likelihood(x, θ), log_likelihood1
