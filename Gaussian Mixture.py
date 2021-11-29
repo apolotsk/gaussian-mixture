e900691f9c111fdd24a0583778090cb0553c0f5a
Alex Polotsk <apolotsk@gmail.com>
2021-11-29 11:38:01 +0200

    Rename "P" to "p"

--- Gaussian Mixture.py
+++ Gaussian Mixture.py
@@ -11,72 +11,72 @@ def gauss(x, mean, stdev):
 
 z_length, x_length = 2, 1000
 def target_θ():
-  P_z = np.expand_dims([0.7, 0.3], axis=1)
+  p_z = np.expand_dims([0.7, 0.3], axis=1)
   means = np.expand_dims([8.0, 13.0], axis=1)
   stdevs = np.expand_dims([1.4, 1.0], axis=1)
-  return P_z, means, stdevs
+  return p_z, means, stdevs
 target_θ = target_θ()
 
 def x(target_θ):
-  P_z, means, stdevs = target_θ
-  z = np.random.choice(z_length, size=x_length, p=P_z[:,0])
+  p_z, means, stdevs = target_θ
+  z = np.random.choice(z_length, size=x_length, p=p_z[:,0])
   samples = np.random.normal(means, stdevs, [z_length, x_length])
   return np.choose(z, samples)
 x = x(target_θ)
 
 def θ():
-  P_z = np.ones([z_length, 1])/z_length
+  p_z = np.ones([z_length, 1])/z_length
   means = np.random.rand(z_length, 1)*20
   stdevs = np.random.rand(z_length, 1)*5
-  return P_z, means, stdevs
+  return p_z, means, stdevs
 θ = θ()
 
 log_likelihood1, log_likelihood0 = float('inf'), float('-inf')
 while abs(log_likelihood1-log_likelihood0)>1e-6:
   def expectation_step(x, θ):
-    P_z, means, stdevs = θ
-    P_x_given_z = gauss(x, means, stdevs)
-    P_z_and_x = P_z*P_x_given_z
-    P_x = P_z_and_x.sum(axis=0, keepdims=True)
-    P_z_given_x = P_z_and_x/P_x
-    return P_z_given_x
-  P_z_given_x = expectation_step(x, θ)
+    p_z, means, stdevs = θ
+    p_x_given_z = gauss(x, means, stdevs)
+    p_z_and_x = p_z*p_x_given_z
+    p_x = p_z_and_x.sum(axis=0, keepdims=True)
+    p_z_given_x = p_z_and_x/p_x
+    return p_z_given_x
+  p_z_given_x = expectation_step(x, θ)
 
-  def maximization_step(P_z_given_x, x):
-    w = P_z_given_x.sum(axis=1, keepdims=True)
-    P_z = w/len(x)
-    means = (P_z_given_x*x).sum(axis=1, keepdims=True)/w
-    stdevs = np.sqrt((P_z_given_x*(x-means)**2).sum(axis=1, keepdims=True)/w)
-    return P_z, means, stdevs
-  θ = maximization_step(P_z_given_x, x)
+  def maximization_step(p_z_given_x, x):
+    w = p_z_given_x.sum(axis=1, keepdims=True)
+    p_z = w/len(x)
+    means = (p_z_given_x*x).sum(axis=1, keepdims=True)/w
+    stdevs = np.sqrt((p_z_given_x*(x-means)**2).sum(axis=1, keepdims=True)/w)
+    return p_z, means, stdevs
+  θ = maximization_step(p_z_given_x, x)
 
-  def P_x(x, θ):
-    P_z, means, stdevs = θ
-    P_x_given_z = gauss(x, means, stdevs)
-    P_z_and_x = P_z*P_x_given_z
-    P_x = P_z_and_x.sum(axis=0)
-    return P_x
+  def p_x(x, θ):
+    p_z, means, stdevs = θ
+    p_x_given_z = gauss(x, means, stdevs)
+    p_z_and_x = p_z*p_x_given_z
+    p_x = p_z_and_x.sum(axis=0)
+    return p_x
 
   def show(x):
     from matplotlib import pyplot
     pyplot.clf()
     pyplot.xlabel('x')
-    pyplot.ylabel('P(x|θ)')
+    pyplot.ylabel('p(x|θ)')
     bin_size = 0.5
     bins = np.arange(x.min(), x.max(), bin_size)
     weights = np.ones_like(x)/len(x)/bin_size
     pyplot.hist(x, bins=bins, weights=weights, color='g', alpha=0.3, label='Samples')
 
     x = np.linspace(x.min(), x.max(), 1000)
-    y = P_x(x, target_θ)
-    pyplot.plot(x, y, 'g-', alpha=0.3, label='P(x)')
+    y = p_x(x, target_θ)
+    pyplot.plot(x, y, 'g-', alpha=0.3, label='p(x)')
 
-    y = P_x(x, θ)
-    pyplot.plot(x, y, 'b-', label='P(x|θ)')
+    y = p_x(x, θ)
+    pyplot.plot(x, y, 'b-', label='p(x|θ)')
 
-    P_z, means, stdevs = θ
+    p_z, means, stdevs = θ
     means, stdevs = means.T, stdevs.T
-    y = P_x(means, θ)
+    y = p_x(means, θ)
     pyplot.plot(means + [[-1],[1]]*stdevs, y/[[2],[2]], '|-b', alpha=0.3, linewidth=1)
     pyplot.plot([[1],[1]]*means, [[0],[1]]*y, '|-b', alpha=0.3, linewidth=1)
 
@@ -85,5 +85,5 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
   show(x)
 
   def log_likelihood(x, θ):
-    return np.log(P_x(x, θ)).mean(axis=0)
+    return np.log(p_x(x, θ)).mean(axis=0)
   log_likelihood1, log_likelihood0 = log_likelihood(x, θ), log_likelihood1
--- Gaussian Mixture.pytorch.py
+++ Gaussian Mixture.pytorch.py
@@ -9,26 +9,26 @@ from torch import tensor
 
 z_length, x_length = 2, 1000
 def target_θ():
-  P_z = tensor(np.expand_dims([0.7, 0.3], axis=1))
+  p_z = tensor(np.expand_dims([0.7, 0.3], axis=1))
   means = tensor(np.expand_dims([8.0, 13.0], axis=1))
   stdevs = tensor(np.expand_dims([1.4, 1.0], axis=1))
-  return P_z, means, stdevs
+  return p_z, means, stdevs
 target_θ = target_θ()
 
 def x(target_θ):
-  P_z, means, stdevs = target_θ
-  P_z, means, stdevs = P_z.numpy(), means.numpy(), stdevs.numpy()
-  z = np.random.choice(z_length, size=x_length, p=P_z[:,0])
+  p_z, means, stdevs = target_θ
+  p_z, means, stdevs = p_z.numpy(), means.numpy(), stdevs.numpy()
+  z = np.random.choice(z_length, size=x_length, p=p_z[:,0])
   samples = np.random.normal(means, stdevs, [z_length, x_length])
   x = np.choose(z, samples)
   return tensor(x)
 x = x(target_θ)
 
 def θ():
-  P_z = tensor(np.ones([z_length, 1])/z_length, requires_grad=True)
+  p_z = tensor(np.ones([z_length, 1])/z_length, requires_grad=True)
   means = tensor(np.random.rand(z_length, 1)*20, requires_grad=True)
   stdevs = tensor(np.random.rand(z_length, 1)*5, requires_grad=True)
-  return P_z, means, stdevs
+  return p_z, means, stdevs
 θ = θ()
 
 def optimizer(parameters):
@@ -38,19 +38,19 @@ optimizer = optimizer(θ)
 
 log_likelihood1, log_likelihood0 = float('inf'), float('-inf')
 while abs(log_likelihood1-log_likelihood0)>1e-6:
-  def P_x(x, θ):
-    P_z, means, stdevs = θ
-    P_z = P_z/P_z.sum(dim=0, keepdim=True)
+  def p_x(x, θ):
+    p_z, means, stdevs = θ
+    p_z = p_z/p_z.sum(dim=0, keepdim=True)
     def gauss(x, mean, stdev):
       from math import pi
       from torch import sqrt, exp
       return 1/sqrt(2*pi*stdev**2) * exp(-(x-mean)**2/(2*stdev**2))
-    P_x_given_z = gauss(x, means, stdevs)
-    P_z_and_x = P_z*P_x_given_z
-    P_x = P_z_and_x.sum(dim=0)
-    return P_x
+    p_x_given_z = gauss(x, means, stdevs)
+    p_z_and_x = p_z*p_x_given_z
+    p_x = p_z_and_x.sum(dim=0)
+    return p_x
   def log_likelihood(x, θ):
-    return torch.log(P_x(x, θ)).mean(dim=0)
+    return torch.log(p_x(x, θ)).mean(dim=0)
   log_likelihood1, log_likelihood0 = log_likelihood(x, θ), log_likelihood1
 
   def update_parameters():
@@ -63,7 +63,7 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
     from matplotlib import pyplot
     pyplot.clf()
     pyplot.xlabel('x')
-    pyplot.ylabel('P(x|θ)')
+    pyplot.ylabel('p(x|θ)')
     bin_size = 0.5
     bins = np.arange(x.min(), x.max(), bin_size)
     weights = np.ones_like(x)/len(x)/bin_size
@@ -71,18 +71,18 @@ while abs(log_likelihood1-log_likelihood0)>1e-6:
     pyplot.hist(x, bins=bins, weights=weights, color='g', alpha=0.3, label='Samples')
 
     x = np.linspace(x.min(), x.max(), 1000)
-    def _P_x(x, θ):
-      with torch.no_grad(): return P_x(tensor(x), θ).numpy()
-    y = _P_x(x, target_θ)
-    pyplot.plot(x, y, 'g-', alpha=0.3, label='P(x)')
+    def _p_x(x, θ):
+      with torch.no_grad(): return p_x(tensor(x), θ).numpy()
+    y = _p_x(x, target_θ)
+    pyplot.plot(x, y, 'g-', alpha=0.3, label='p(x)')
 
-    y = _P_x(x, θ)
-    pyplot.plot(x, y, 'b-', label='P(x|θ)')
+    y = _p_x(x, θ)
+    pyplot.plot(x, y, 'b-', label='p(x|θ)')
 
-    P_z, means, stdevs = θ
+    p_z, means, stdevs = θ
     means, stdevs = means.T, stdevs.T
     with torch.no_grad(): means, stdevs = means.numpy(), stdevs.numpy()
-    y = _P_x(means, θ)
+    y = _p_x(means, θ)
     pyplot.plot(means + [[-1],[1]]*stdevs, y/[[2],[2]], '|-b', alpha=0.3, linewidth=1)
     pyplot.plot([[1],[1]]*means, [[0],[1]]*y, '|-b', alpha=0.3, linewidth=1)
 
